fac8e0b3f1a56726508a460f747b61b5af310c69
unknown <d_bille@yahoo.com>
2020-08-04 15:05:41 -0500

    setting up mongoDB store schema

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,11 +1,3 @@
-const { excludedProperties } = require("juice");
-exports.myMiddleware = (req, res, next) => {
-    req.name = 'David'
-    next()
-};
-
 exports.homePage = (req, res) => {
-  res.render("index", {
-    name: req.name,
-  });
-};
+    res.render('index')
+}
