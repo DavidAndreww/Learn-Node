944f62fe1427ed7cbe260b7d0426ae22a5b803f2
unknown <d_bille@yahoo.com>
2020-08-04 18:52:34 -0500

    created form using mixin, added it to pug file to display. working on sending data form form to appropriate path.

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,3 +1,17 @@
 exports.homePage = (req, res) => {
     res.render('index')
 }
+
+exports.addStore = (req, res) => {
+    res.render('editStore', {
+        title: 'Add Store'
+    })
+}
+
+exports.createStore = (req, res)=>{
+    console.log('create store')
+    res.json(req.body)
+    // res.render('index', {
+    //     store: req.body.name
+    // })
+}
