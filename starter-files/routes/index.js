82bcb678a62a06f5beb33fb133b4a9de4af24f04
unknown <d_bille@yahoo.com>
2020-08-04 12:57:04 -0500

    learning how to use pug to res.render() site template and update dynamically with blocks

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,8 +3,14 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works! Crikey!');
+  // res.send('Hey! It works! Crikey!');
   // res.json(req.query)
+  let pathData = {
+    name: req.query.name,
+    job: req.query.job,
+    location: req.query.location
+  }
+  res.render('hello',pathData)
 });
 
 module.exports = router;
