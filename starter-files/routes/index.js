5c8be0f3a3e6eb810e9eafead3c5b9cd7cf3eca3
unknown <d_bille@yahoo.com>
2020-08-04 13:40:02 -0500

    locals and adding middleware to import all helper exports into our pug files

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // res.send('Hey! It works! Crikey!');
   // res.json(req.query)
-  let pathData = {
+  res.render("hello", {
     name: req.query.name,
     job: req.query.job,
-    location: req.query.location
-  }
-  res.render('hello',pathData)
+    location: req.query.location,
+  });
+  console.log(res.locals)
 });
 
 module.exports = router;
